a2db609915db4541505e0b24c848544bdaf58423
Merve Şimşek <93996187+Merve-Simsek92@users.noreply.github.com>
2022-03-26 19:19:55 +0300

    commit

---
+++ solution/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './compenents/header/Header';
+import Card from './compenents/card/Card';
+
+function App() {
+  return (
+    <div className="main-container">
+     <Header/>
+     <Card/>
+    </div>
+  );
+}
+
+export default App;
